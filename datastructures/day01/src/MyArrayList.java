a16f8792080bd19b32a0b80bb4e4f306b567b103
AmyPhung <amyngph@gmail.com>
2019-01-30 09:41:59 -0500

    Complete day01

--- datastructures/day01/src/MyArrayList.java
+++ datastructures/day01/src/MyArrayList.java
@@ -2,41 +2,88 @@ public class MyArrayList {
     private Cow[] elems;
     private int size;
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N) - calls constructor which is O(N)
     public MyArrayList() {
-        // TODO
+        this(10); // calls the other constructor
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N) - needs to allocate space for N objects
     public MyArrayList(int capacity) {
-        // TODO
+        elems = new Cow[capacity];
+        size = 0;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N)* - Resizing is N average and adding is N worst case
     public void add(Cow c) {
-        // TODO
+        // if array is full, double size.
+        if (size == elems.length) {
+            Cow[] newArr = new Cow[elems.length * 2];
+            System.arraycopy(elems, 0, newArr, 0, size);
+            elems = newArr;
+        }
+
+        elems[size] = c;
+        size++;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1) - returns saved variable
     public int size() {
-        // TODO
-        return -1;
+        return size;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(1) - returns saved object
     public Cow get(int index) {
-        // TODO
-        return null;
+        if (index >= size) {
+            //index out of bounds exception
+            throw new IndexOutOfBoundsException(index);
+        } else {
+            return elems[index];
+        }
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N)* - N worst case for removing and N average for resizing
     public Cow remove(int index) {
-        // TODO
-        return null;
+        Cow rm;
+        rm = elems[index];
+
+        // shift all elements after removed index back
+        for (int i=index; i<elems.length-1; i++) {
+          elems[i] = elems[i + 1];
+        }
+
+        size--;
+
+        // if array is less than 25% full, reduce to half size - don't resize to length 1
+        if (size < elems.length*0.25) {
+            Cow[] newArr = new Cow[elems.length/2];
+            if (elems.length/2 != 1) {
+            System.arraycopy(elems, 0, newArr, 0, size);
+            elems = newArr;
+          }
+        }
+        return rm;
     }
 
-    // TODO: Runtime: O(?)
+    // TODO: Runtime: O(N)* - N average for resizing and N worst case for shifting elements
     public void add(int index, Cow c) {
-        // TODO
+        if (index > size) {
+          //index out of bounds exception
+          throw new IndexOutOfBoundsException(index);
+        }
+
+        // if array is full, double size.
+        if (size == elems.length) {
+            Cow[] newArr = new Cow[elems.length*2];
+            System.arraycopy(elems, 0, newArr, 0, size);
+            elems = newArr;
+        }
+        size++;
+
+        // move all elements after insertion
+        for (int i=size; i>index; i--)  {// fix syntax
+          elems[i] = elems[i-1];
+        }
+
+        elems[index] = c;
     }
 }
