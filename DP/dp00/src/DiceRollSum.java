0806f801267849744a903488fe6e8d1595e7dfdb
AmyPhung <amyngph@gmail.com>
2019-04-27 10:11:41 -0400

    Complete DP 00

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,9 +1,48 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
     // Runtime: TODO
     // Space: TODO
+
+
+
     public static int diceRollSum(int N) {
         // TODO
+        HashMap<Integer, Integer> DP = new HashMap<>();
+        return diceRollSumHelper(N, DP);
+    }
+
+    private static int diceRollSumHelper(int N, HashMap<Integer,Integer> DP) {
+        if (DP.containsKey(N)) {
+            return DP.get(N);
+        } else if (N < 1) {
+            return 1;
+        } else if (N <= 6) {
+            switch (N) {
+                case 1:
+                    return 1;
+                case 2:
+                    return 2;
+                case 3:
+                    return 4;
+                case 4:
+                    return 8;
+                case 5:
+                    return 16;
+                case 6:
+                    return 32;
+            }
+        } else {
+            int new_value = diceRollSum(N-1) +
+                    diceRollSum(N-2) +
+                    diceRollSum(N-3) +
+                    diceRollSum(N-4) +
+                    diceRollSum(N-5) +
+                    diceRollSum(N-6);
+            DP.put(N, new_value);
+            return DP.get(N);
+        }
         return 0;
     }
 
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,55 @@
+import java.util.HashMap;
+
 public class LongestIncreasingSubsequence {
 
     // Runtime: TODO
     // Space: TODO
     public static int LIS(int[] A) {
         // TODO
-        return 0;
+        HashMap<Integer,Integer> DP = new HashMap<>();
+
+        int max_length = 0;
+        for (int i=A.length-1; i>=0; i--) {
+            int count_from_index = LISHelper(A, i, DP);
+            if (count_from_index > max_length) {
+                max_length = count_from_index;
+            }
+        }
+
+        return max_length;
+    }
+
+    private static int findNextLargest(int[] A, int i, HashMap<Integer,Integer> DP) {
+        int max_idx = -1;
+        int max_val = 0;
+        for (int idx=i+1; idx<A.length; idx++) {
+            if (A[idx] > A[i]) {
+                int next_val = LISHelper(A, idx, DP);
+                if (next_val > max_val) {
+                    max_idx = idx;
+                    max_val = next_val;
+                }
+            }
+        }
+        return max_idx;
+    }
+
+    private static int LISHelper(int[] A, int i,  HashMap<Integer,Integer> DP) {
+        if (DP.containsKey(i)) {
+            return DP.get(i);
+        } else if (i == A.length - 1) {
+            return 1; // Return 1 for the last element in subsequence
+        } else {
+            int next = findNextLargest(A, i, DP);
+
+            if (next == -1) {
+                DP.put(i, 1);
+                return 1; // Return 1 if there is no higher element
+            } else {
+                int next_value = LISHelper(A, next, DP);
+                DP.put(i, next_value + 1);
+                return next_value + 1;
+            }
+        }
     }
 }
